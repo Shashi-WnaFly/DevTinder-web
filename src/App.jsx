b98bc03475f7d7207da8235a4845ac27da846d84
Shashi-Anand <shashianand2600@gmail.com>
2025-07-30 19:29:58 +0530

    install tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,12 @@ import './App.css'
 function App() {
 
   return (
-    <>
-      <h1>Hello ji</h1>
-    </>
+    <div>
+      <h1 class="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+
+    </div>
   )
 }
 
