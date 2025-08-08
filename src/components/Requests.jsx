10f24a9da50e903961598eb748a6e2d31a5acafc
Shashi-Anand <shashianand2600@gmail.com>
2025-08-08 20:27:13 +0530

    writing review request click logic

--- src/components/Requests.jsx
+++ src/components/Requests.jsx
@@ -19,9 +19,18 @@ const Requests = () => {
     }
   };
 
+  const handleReview = async (status, requestId) => {
+    try {
+      const review = await axios.post(`${BASE_URL}/request/review/${status}/${requestId}`, {}, {withCredentials: true});
+      console.log(review);
+    } catch (error) {
+      console.log(""+error);
+    }
+
+  }
+
   useEffect(() => {
-    if(!reqs)
-      fetchRequests();
+    if (!reqs) fetchRequests();
   }, []);
 
   return (
@@ -30,15 +39,30 @@ const Requests = () => {
       return (
         <div
           key={row._id}
-          className="flex h-[10rem] w-6/12 mx-auto justify-between p-10 bg-base-300"
+          className="flex h-[10rem] w-6/12 mx-auto items-center justify-between p-4 bg-base-300"
         >
-          <div className="w-24">
-            <img src={row.fromUserId.photoUrl} alt="profilePhoto" />
+          <div className="flex items-center gap-2">
+            <div className="w-24 rounded-full outline-6 outline-black">
+              <img className="rounded-full" src={row.fromUserId.photoUrl} alt="profilePhoto" />
+            </div>
+            <div >
+              <p>{row.fromUserId.firstName + " " + row.fromUserId.lastName}</p>
+              <p>{row.fromUserId.gender + " " + row.fromUserId.age} </p>
+              <p>{row.fromUserId.about}</p>
+            </div>
           </div>
-          <div>
-            <p>{row.fromUserId.firstName + " " + row.fromUserId.lastName}</p>
-            <p>{row.fromUserId.gender + " " + row.fromUserId.age} </p>
-            <p>{row.fromUserId.about}</p>
+
+          <div className="flex flex-col gap-2">
+            <button onClick={() => {
+              handleReview("rejected", row._id);
+            }} className="btn btn-primary px-8 rounded-lg w-24">
+              Reject
+            </button>
+            <button onClick={() => {
+              handleReview("accepted", row._id);
+            }} className="btn btn-primary px-8 rounded-lg w-24">
+              Accept
+            </button>
           </div>
         </div>
       );
