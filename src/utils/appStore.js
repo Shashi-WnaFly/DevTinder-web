b0e3cf67abe9815a93ec5ee4b831639b722576fb
Shashi-Anand <shashianand2600@gmail.com>
2025-08-08 21:17:34 +0530

    create connection local store

--- src/utils/appStore.js
+++ src/utils/appStore.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import userSlice from "./userSlice";
 import feedSlice from "./feedSlice";
 import requestSlice from "./requestSlice";
+import connectionSlice from "./connectionSlice";
 
 const appStore = configureStore({
     reducer: {
         user: userSlice,
         feed: feedSlice,
         requests: requestSlice,
+        connections: connectionSlice,
     }
 });
 
