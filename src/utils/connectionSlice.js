ae98492d99fd22c347b9ba0f39ddb2cd47699506
Shashi-Anand <shashianand2600@gmail.com>
2025-09-18 21:33:12 +0530

    connectionSlice bug fix

--- src/utils/connectionSlice.js
+++ src/utils/connectionSlice.js
@@ -1,6 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const connectionSlice = new createSlice({
+const connectionSlice = createSlice({
     name: "connections",
     initialState: null,
     reducers: {
