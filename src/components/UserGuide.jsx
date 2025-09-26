de4f6612e7ce1b3ae45fbf284550724e2bae2eb6
Shashi-Anand <shashianand2600@gmail.com>
2025-09-26 22:57:49 +0530

    user guide page is complete

--- src/components/UserGuide.jsx
+++ src/components/UserGuide.jsx
@@ -4,46 +4,177 @@ const UserGuide = () => {
   return (
     <div className="flex flex-col justify-center items-center">
       <h1 className="font-semibold text-lg">Agreement</h1>
-      <div className="overflow-x-hidden flex flex-col gap-4">
-        <div className="font-semibold">Subscription Policy</div>
-        <div>
-          By starting the Premium Subscription, you agree to our Terms of
-          Service and Privacy Statement. You may cancel at any time during your
-          subscription.
+
+      <div className="flex flex-col gap-4 text-gray-300 text-sm/6">
+        <div className="flex flex-col gap-2">
+          <h1 className="font-semibold text-gray-50 text-base">
+            Subscription Policy
+          </h1>
+          <p>
+            By starting the Premium Subscription, you agree to our{" "}
+            <span className="text-gray-200 font-bold">
+              Terms of Service and Privacy Statement
+            </span>
+            . You may cancel at any time during your subscription.
+          </p>
+
+          <p>
+            <span className="text-gray-200 font-bold">
+              There are no refunds for any amounts that have been charged for
+              subscription.
+            </span>{" "}
+            Your subscription will stay active until the end of billing period.
+          </p>
+          <p>
+            Devtinder may deny you access to all or any part of the Devtinder
+            service or terminate your account with or without prior notice if
+            you engage in any conduct or activities that Devtinder determines,
+            in its sole discretion, violates Devtinder's Terms of Service or
+            Devtinder's rules or is otherwise inappropriate. Without limitation,
+            Devtinder may deny you access to Devtinder service or terminate your
+            Devtinder account, without providing any refund or partial refund.
+          </p>
         </div>
-        <div>
-          To cancel, go to Billing (https://leetcode.com/subscription/) and
-          click on "Cancel". Once canceled, your subscription will stay active
-          until the end of billing period.
+        <div className="flex flex-col gap-2">
+          <h1 className="font-semibold text-gray-50 text-base">
+            Acceptable Use Policy
+          </h1>
+          <div>
+            Devtinder is a developer version of Tinder, location-based social
+            discovery and dating app that allows users to connect with new
+            people by swiping right to like or left to dislike profiles. If two
+            users mutually like each other's profiles, it's a match, and they
+            can begin chatting and arranging to meet in real life. Users can
+            create profiles with photos and descriptions, find people with
+            similar interests, and find friends, dates, or relationships.
+          </div>
+          <div>
+            Although we do not routinely screen or monitor content provided by
+            users, we may remove or edit inappropriate content or activity
+            reported to us or suspend, disable, or terminate a user's access to
+            all or part of the Services.
+          </div>
         </div>
-        <div>
-          eetCode will automatically renew your subscription at the end of your
-          billing period and charge to your payment method on a "period" basis
-          until you cancel. There are no refunds for any amounts that have been
-          charged for subscription cancellation.
+        <div className="flex flex-col gap-4">
+          <h1 className="font-semibold text-gray-50 text-base my-2">
+            You are prohibited from using our Services to share content that:
+          </h1>
+          <div className="flex">
+            <span className="before:content-['•'] before:text-purple-400 text-xl pr-5"></span>
+            <p>
+              Contains illegal content or promotes illegal activities with the
+              intent to commit such activities. Contains credible threats or
+              organizes acts of real-world violence. We don't allow content that
+              creates a genuine risk of physical injury or property damage,
+              credibly threatens people or public safety, or organizes or
+              encourages harm.
+            </p>
+          </div>
+          <div className="flex">
+            <span className="before:content-['•'] before:text-purple-400 text-xl pr-5"></span>
+            <p>
+              Harrasses others. We encourage commentary about people and matters
+              of public interest, but abusive or otherwise inappropriate content
+              directed at private individuals is not allowed.
+            </p>
+          </div>
+          <div className="flex">
+            <span className="before:content-['•'] before:text-purple-400 text-xl pr-5"></span>
+            <p>
+              Violates intellectual property, privacy, or other rights. Do not
+              share content that you do not have the right to share, claim
+              content that you did not create as your own, or otherwise infringe
+              or misappropriate someone else's intellectual property or other
+              rights. Always attribute materials used or quoted by you to the
+              original copyright owner.
+            </p>
+          </div>
+          <div className="flex">
+            <span className="before:content-['•'] before:text-purple-400 text-xl pr-5"></span>
+            <p>
+              Spams others. Do not share irrelevant or inappropriate
+              advertising, promotional, or solicitation content.
+            </p>
+          </div>
+          <div className="flex">
+            <span className="before:content-['•'] before:text-purple-400 text-xl pr-5"></span>
+            <p>
+              Otherwise violates the LeetCode Terms of Use. Please note that
+              specific courses may have additional rules and requirements.
+            </p>
+          </div>
         </div>
-        <div>
-          LeetCode may deny you access to all or any part of the LeetCode
-          service or terminate your account with or without prior notice if you
-          engage in any conduct or activities that LeetCode determines, in its
-          sole discretion, violates LeetCode's Terms of Service or LeetCode's
-          rules or is otherwise inappropriate. Without limitation, LeetCode may
-          deny you access to LeetCode service or terminate your LeetCode
-          account, without providing any refund or partial refund.
-        </div>
-        <div>
-          LeetCode is a platform for people to expand knowledge and get prepared
-          for the next technical coding interviews, it helps people level up
-          coding skills and quickly land a job. We want to make sure that all of
-          our users and instructors feel safe and comfortable while using our
-          Services. We have drafted these guidelines to ensure that people
-          understand and follow the rules when using our services.
-        </div>
-        <div>
-          Although we do not routinely screen or monitor content provided by
-          users, we may remove or edit inappropriate content or activity
-          reported to us or suspend, disable, or terminate a user's access to
-          all or part of the Services.
+        <div className="flex flex-col gap-4">
+          <h1 className="font-semibold text-gray-50 text-base my-2">
+            You also aren't allowed to:
+          </h1>
+          <div className="flex">
+            <span className="before:content-['•'] before:text-purple-400 text-xl pr-5"></span>
+            <p>
+              Do anything that violates local, state, national or international
+              law or breaches any of your contractual obligations or fiduciary
+              duties.
+            </p>
+          </div>
+          <div className="flex">
+            <span className="before:content-['•'] before:text-purple-400 text-xl pr-5"></span>
+            <p>
+              Share your password, let anyone access your account, or do
+              anything that might put your account at risk.
+            </p>
+          </div>
+          <div className="flex">
+            <span className="before:content-['•'] before:text-purple-400 text-xl pr-5"></span>
+            <p>Attempt to access any other user's account.</p>
+          </div>
+          <div className="flex">
+            <span className="before:content-['•'] before:text-purple-400 text-xl pr-5"></span>
+            <p>
+              Reproduce, transfer, sell, resell, or otherwise misuse any content
+              from our Services, unless specifically authorized to do so.
+            </p>
+          </div>
+          <div className="flex">
+            <span className="before:content-['•'] before:text-purple-400 text-xl pr-5"></span>
+            <p>
+              Access, tamper with, or use non-public areas of our systems,
+              unless specifically authorized to do so.
+            </p>
+          </div>
+          <div className="flex">
+            <span className="before:content-['•'] before:text-purple-400 text-xl pr-5"></span>
+            <p>
+              Break or circumvent our authentication or security measures or
+              otherwise test the vulnerability of our systems or networks,
+              unless specifically authorized to do so.
+            </p>
+          </div>
+          <div className="flex">
+            <span className="before:content-['•'] before:text-purple-400 text-xl pr-5"></span>
+            <p>Try to reverse engineer any portion of our Services.</p>
+          </div>
+          <div className="flex">
+            <span className="before:content-['•'] before:text-purple-400 text-xl pr-5"></span>
+            <p>
+              Try to interfere with any user, host, or network, for example by
+              sending a virus, overloading, spamming, or mail-bombing.
+            </p>
+          </div>
+          <div className="flex">
+            <span className="before:content-['•'] before:text-purple-400 text-xl pr-5"></span>
+            <p>Use our Services to distribute malware.</p>
+          </div>
+          <div className="flex">
+            <span className="before:content-['•'] before:text-purple-400 text-xl pr-5"></span>
+            <p>
+              Impersonate or misrepresent your affiliation with any person or
+              entity.
+            </p>
+          </div>
+          <div className="flex">
+            <span className="before:content-['•'] before:text-purple-400 text-xl pr-5"></span>
+            <p>Encourage or help anyone do any of the things on this list.</p>
+          </div>
         </div>
       </div>
     </div>
