0c2075d979ba2ec27b55fc993231542cdecd395f
Shashi-Anand <shashianand2600@gmail.com>
2025-09-29 21:52:30 +0530

    create Privacy and Policy

---
+++ src/components/Privacy.jsx
@@ -0,0 +1,333 @@
+import React from "react";
+import Footer from "./Footer";
+
+const Privacy = () => {
+  return (
+    <div className="bg-zinc-50 text-gray-600">
+      <div className="max-w-[70rem] mx-auto px-4 text-[17px]">
+        <h1 className="text-center text-3xl py-10">
+          <span className="text-gray-700 font-light">Devtinder</span>{" "}
+          <span className="font-semibold">Privacy Policy</span>
+        </h1>
+        <hr className="text-gray-300 my-8" />
+        <p>
+          Devtinder, LLC ("we”, “us”, or “our") provides this Privacy Policy to
+          inform you of our policies and procedures regarding the collection,
+          use and disclosure of personal information we may receive from users
+          of our website (“Site”) and any other services offered by us in
+          connection with our site (any and all of the foregoing the
+          "Services").
+        </p>
+        <div>
+          <h1 className="my-8 text-2xl font-semibold text-black">
+            INFORMATION COLLECTION: PERSONALLY IDENTIFIABLE INFORMATION
+          </h1>
+          <p>
+            In the course of using the Services, we collect personally
+            identifiable information, in the form of Persistent Identifiers,
+            including IP Address. We collect and stores this personal
+            information solely for the purpose of providing support for the
+            internal operations of our Services and our Site.
+          </p>
+        </div>
+        <div>
+          <h1 className="my-8 text-2xl font-semibold text-black">
+            COOKIES AND OTHER TECHNOLOGIES
+          </h1>
+          <p>
+            Like many websites and applications, we use ''cookies'' to collect
+            information. A cookie is a small data file that we transfer to your
+            computer's hard disk for record-keeping purposes. We may use
+            ''session ID cookies'' to enable certain features of the Site or
+            Services, to better understand how you interact with the Site and to
+            monitor web traffic routing on the site and aggregate usage of the
+            site and services. You can instruct your browser, by changing its
+            options, to stop accepting cookies or to prompt you before accepting
+            a cookie from the websites you visit. If you do not accept cookies,
+            however, you may not be able to use all portions of the Site or all
+            functionality of the Services.
+          </p>
+        </div>
+        <div className="flex flex-col gap-4">
+          <h1 className="mt-4 mb-2 text-2xl font-semibold text-black">
+            INFORMATION SHARING AND DISCLOSURE
+          </h1>
+          <p>
+            WE WILL NOT SHARE, SELL, RENT, OR TRADE YOUR PERSONAL INFORMATION
+            WITH OTHER PARTIES EXCEPT AS PROVIDED BELOW:
+          </p>
+          <p>
+            Through our terms and services, you have consented to the
+            dissemination of this information to the third parties. Such
+            information shall be limited to your name as provided with your
+            account, the amount associated with the use of the account, and when
+            any such activity took place. If you do not consent to this
+            information, we will provide you with an opportunity to opt out of
+            the sharing of the information.
+          </p>
+          <p>
+            Personal Information: We may share personal information including IP
+            Address, for the purpose of providing support for the internal
+            operations of our services and our site.
+          </p>
+          <p>
+            "Support for the internal operations of our Services and our Site"
+            means activities necessary for the services or site to maintain or
+            analyze its functioning; perform network communications;
+            authenticate users or personalize content; serve contextual
+            advertising or cap the frequency of advertising; protect the
+            security or integrity of the user, service, or site; ensure legal or
+            regulatory compliance; or perform other usage analytics. Through our
+            services and site, no other personal information is collected and
+            the persistent identifiers are not used or disclosed to contact a
+            specific individual, including through behavioral advertising; to
+            amass a profile on a specific individual; or for any other purpose.
+          </p>
+          <p>
+            Aggregate Information and Non-Identifying Information. We may share
+            aggregated information that includes non-identifying information and
+            Log Data with third parties for industry analysis, demographic
+            profiling, and to deliver contextual advertising about other
+            products and services.
+          </p>
+          <p>
+            Services Providers. We employ third party companies and individuals
+            to facilitate our services (such as maintenance services, database
+            management, web analytics and improvement of our Services'
+            features), to provide the services on our behalf, or to assist us in
+            analyzing how services are used. These third parties have access to
+            your personal information only to perform these tasks on our behalf
+            and are obligated not to disclose or use it for any other purpose.
+          </p>
+          <p>
+            Compliance with Laws and Law Enforcement. We may disclose personal
+            information if required to do so by law or in the good-faith belief
+            that such action is necessary to comply with state and federal laws
+            (such as U.S. Copyright law) or respond to a court order, judicial
+            or other government subpoena, or warrant in the manner required by
+            the requesting entity.
+          </p>
+          <p>
+            We also reserve the right to disclose information that we believe,
+            in good faith, is appropriate or necessary to take precautions
+            against liability; protect us from fraudulent, abusive, or unlawful
+            uses; to investigate and defend ourselves against third-party claims
+            or allegations; to assist government enforcement agencies; to
+            protect the security or integrity of the Services or Site; enforce
+            our Terms of Service; or to protect the rights, property, or
+            personal safety of us, our users, or others.
+          </p>
+        </div>
+        <div>
+          <h1 className="my-8 text-2xl font-semibold text-black">
+            BUSINESS TRANSFERS
+          </h1>
+          <p>
+            We may sell, transfer or otherwise share some or all of its assets,
+            including your Personal Information, in connection with a merger,
+            acquisition, reorganization or sale of assets or in the event of
+            bankruptcy. Such transfer will comply with all applicable laws and
+            regulations.
+          </p>
+        </div>
+        <div>
+          <h1 className="my-8 text-2xl font-semibold text-black">SECURITY</h1>
+          <p>
+            We are concerned with the security of your personal information and
+            are committed to taking reasonable steps to protect it from
+            unauthorized access and use. To that end, we put in place the
+            appropriate physical, electronic and managerial policies and
+            procedures designed to secure Personal Information. We also continue
+            to implement procedures to maintain accurate, complete and current
+            Personal Information. No method of transmission over the Internet,
+            or method of electronic storage, is one hundred percent secure.
+            Therefore, while we strive to use commercially acceptable means to
+            protect Personal Information, we cannot guarantee its absolute
+            security.
+          </p>
+          <p>
+            We will make any legally required disclosures of any breach of the
+            security, confidentiality, or integrity of your unencrypted
+            electronically stored personal information (as defined in applicable
+            state statutes on security breach notification) to you via email or
+            conspicuous posting on the site in the most expedient time possible
+            and without unreasonable delay, insofar as consistent with (i) the
+            legitimate needs of law enforcement or (ii) any measures necessary
+            to determine the scope of the breach and restore the reasonable
+            integrity of the data system.
+          </p>
+        </div>
+        <div>
+          <h1 className="my-8 text-2xl font-semibold text-black">
+            INTERNATIONAL TRANSFER
+          </h1>
+          <p>
+            Your information may be transferred to - and maintained on -
+            computers located outside of your state, province, country or other
+            governmental jurisdiction where the privacy laws may not be as
+            protective as those in your jurisdiction. If you are located outside
+            the United States and choose to provide information to us, we may
+            transfer the personal information to the United States and processes
+            it there. Use of our services or site represents your consent to
+            this Privacy Policy and your agreement to that transfer.
+          </p>
+        </div>
+        <div>
+          <h1 className="my-8 text-2xl font-semibold text-black">
+            LINKS TO OTHER SITES
+          </h1>
+          <p>
+            Our services may contain links to other websites, applications, and
+            online services. If you choose to visit a third party service or
+            click on another third party link, you will be directed to that
+            third party's website, application, or online service. The fact that
+            we link to a website, content is not an endorsement, authorization
+            or representation of our affiliation with that third party, nor is
+            it an endorsement of their privacy or information security policies
+            or practices. We do not exercise control over third party websites
+            or services. These other websites or services may place their own
+            cookies or other files on your computer, collect data or solicit
+            personal information from you. Other websites and services follow
+            different rules regarding the use or disclosure of the personal
+            information you submit to them. We encourage you to read the privacy
+            policies or statements of the other websites and services you use.
+          </p>
+        </div>
+        <div className="flex flex-col gap-4">
+          <h1 className="mt-8 mb-4 text-2xl font-semibold text-black">
+            ACCOUNT DELETION
+          </h1>
+          <p>
+            You can request to delete your Devtinder Account anytime. Before you
+            proceed with the account deletion process, please read and fully
+            understand the terms related to account deletion. If you do not
+            agree with the terms, please stop the deletion process immediately.
+          </p>
+          <h3 className="my-4 text-lg font-semibold text-black">
+            About deleting your Devtinder account:
+          </h3>
+          <p>
+            1. Once you request to delete your Devtinder account, you will enter
+            a 15-day cool-off period during which we will preserve your data but
+            it will not be visible to other users. You can retrieve your account
+            anytime by logging back into Devtinder within the cool-off period.
+          </p>
+          <p>
+            2. If an account is permanently deleted after the cool-off period,
+            such deletion is irreversible, and you will not regain access. All
+            third-party accounts connected to this deleted account will be
+            automatically disconnected, and you will not be able to log in with
+            these third-party accounts.
+          </p>
+          <p>
+            3. Upon deletion, you will lose access to all content, including
+            information, data, and records associated with your account. You
+            will lose access to all subscriptions and or/content you purchased
+            with that account. You will not be able to recover or request to
+            retrieve any account-related content or information, even if you
+            register and use Devtinder again with the same user name or email
+            address.
+          </p>
+          <p>
+            4. Some content, such as comments or posts you made in the
+            community, may remain visible to other users after your account
+            deletion, but this information will be displayed anonymously.
+          </p>
+          <p>
+            5. Once you agree to delete, the relevant terms of service and other
+            rights with Devtinder will terminate accordingly, except for those
+            that have been agreed to continue in effective or as otherwise
+            prescribed by laws and regulations. You will also no longer enjoy
+            the user rights outlined in the "Privacy Policy," except as
+            otherwise mandated by laws and regulations or required by regulatory
+            authorities.
+          </p>
+          <h3 className="my-4 text-lg font-semibold text-black">
+            Before Requesting Deletion of Your Devtinder Account:
+          </h3>
+          <p>Ensure that the account meets the following conditions:</p>
+          <p>
+            • It is registered through our official channels, complies with the
+            "Devtinder Terms of Service," and is registered by you;
+          </p>
+          <p>• There has been no abnormal activity in the last 3 months;</p>
+          <h4>• There are no unused virtual assets, including:</h4>
+          <div className="pl-8 flex flex-col gap-4">
+            <p> ◦ No incomplete orders in the Devtinder Store;</p>
+            <p> ◦ No active Devtinder Premium subscriptions;</p>
+            <p> ◦ No unused credits;</p>
+            <p> ◦ No purchased explore cards.</p>
+          </div>
+          <p>• If there is an active subscription, please cancel it.</p>
+          <h3 className="my-4 text-lg font-semibold text-black">
+            Process for Deleting a Devtinder Account:
+          </h3>
+          <p>
+            1. Initiate your account deletion request at
+            https://tinderdev.club/profile/account/.
+          </p>
+          <p>2. Read and agree to the Account Deletion terms.</p>
+          <p>3. Submit your account deletion request.</p>
+          <p>4. Enter a 15-day cool-off period.</p>
+          <p>
+            5. After 15 days, your account will be permanently deleted, and you
+            will lose all content and information associated with that account.
+          </p>
+          <p>
+            If we have reasonable grounds to suspect or discover that your
+            account is engaged in activities violating our Terms of Service,
+            Privacy Policies, or any applicable regulations, we reserve the
+            right to refuse your deletion request, halt the account deletion
+            process, or prohibit the registration of a new user account with the
+            same email associated with your account.
+          </p>
+          <h3 className="my-4 text-lg font-semibold text-black">
+            Account Deletion Restriction Notice:
+          </h3>
+          <p>
+            To uphold the security of your account and protect your rights of
+            property, we cannot process with account deletion requests if any of
+            the following conditions apply:
+          </p>
+          <p>• You currently maintain an active premium membership status.</p>
+          <p>• Your account has a credit balance greater than zero.</p>
+          <p>• There are active, processing orders in your store account.</p>
+          <p>
+            • Your account includes any purchased courses or digital assets.
+          </p>
+          <p>
+            If you wish to proceed with account deletion under these
+            circumstances, please contact our support team for assistance.
+          </p>
+        </div>
+        <div>
+          <h1 className="mt-8 mb-4 text-2xl font-semibold text-black">CHANGES AND UPDATES TO THIS PRIVACY POLICY</h1>
+          <p>
+            This Privacy Policy may be revised periodically, and this will be
+            reflected by a ''last modified'' date above. Please revisit this
+            page to stay aware of any changes. Our amended Privacy Policy will
+            automatically take effect 30 days after the change, except that we
+            will not, without your consent, use your previously collected
+            personally identifiable information in a manner inconsistent the
+            Privacy Policy in effect when we received that information. If you
+            do not agree with any changes to the Privacy Policy, you may
+            terminate your use of the Services.
+          </p>
+        </div>
+        <div>
+          <h1 className="mt-8 mb-4 text-2xl font-semibold text-black">CONTACT INFORMATION</h1>
+          <p>
+            Please contact us with any questions or comments about this Privacy
+            Policy, Our Privacy Policy Toward Children, your personal
+            information, and our third-party disclosure practices, at
+            support@devtinder.com
+          </p>
+        </div>
+        <p className="text-right mt-8">Last modified: 29/09/2025</p>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Privacy;
